18902994e0f1d984cb08a5bb64865a3b244016f6
Kevin Flanagan <kevin@kflan.io>
2020-03-02 13:01:55 -0700

    starter, auth added

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -6,7 +6,6 @@ const UserContext = React.createContext();
 
 function UserProvider(props) {
   const { user } = useAuth();
-  console.log('user userContext: ', user);
 
   const [firstAttemptFinished, setFirstAttemptFinished] = React.useState(false);
 
